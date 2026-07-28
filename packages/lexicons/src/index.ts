842592af814dd0303552512fbc52e95241193c1c
kevinkiklee <kevin.kik.lee@gmail.com>
2026-07-28 11:28:43 -0400

    feat: add packages/lexicons with opencontent record type constants

---
+++ packages/lexicons/src/index.ts
@@ -0,0 +1,3 @@
+export const OPENCONTENT_PHOTOGRAPH = "social.opencontent.photograph";
+export const OPENCONTENT_COLLECTION = "social.opencontent.collection";
+export const OPENCONTENT_SITE = "social.opencontent.site";
